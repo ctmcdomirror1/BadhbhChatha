12932a5be01a044bfed1cdf04ea167af0439e002
Colin McDonagh <24464477+ctmcdo@users.noreply.github.com>
2023-02-03 18:18:36 +0000

    Remove mistakenly added files

--- AdieuMyJess/gmp_version.py
+++
@@ -1,7 +0,0 @@
-import ctypes
-so_name='/usr/local/lib/libgmp.so' # or /usr/lib64/libgmp.so, etc
-var_name='__gmp_version'
-L=ctypes.cdll.LoadLibrary(so_name)
-v=ctypes.c_char_p.in_dll(L,var_name)
-print(v.value)
-
